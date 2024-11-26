1f5c2c38634dc946fa28614f95dee2acdc5f0ac6
nbayliss <nigel.bayliss@oracle.com>
2024-11-26 09:25:58 +0000

    Clarify hint comment text

--- automatic_spm/adb-real-time/step2_dbrm.sql
+++ automatic_spm/adb-real-time/step2_dbrm.sql
@@ -3,7 +3,7 @@
 @@plan_bad
 --
 -- The Note section indicates invalidations
--- We've manipulated to cost to cause a plan performance
+-- We've manipulated the plan with hints to cause a performance
 -- regression. Real-time SPM will detect the plan change
 -- and reinstate the old plan.
 --
