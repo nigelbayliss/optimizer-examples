06e81e6be8f1022cdab57061e838121efdcda45b
nbayliss <nigel.bayliss@oracle.com>
2025-09-25 11:07:35 +0100

    Add instance number

--- spm_awr/load_awr.sql
+++ spm_awr/load_awr.sql
@@ -15,6 +15,7 @@ prompt
 select dbms_sqltune.sqltext_to_signature(sql_text) signature
 from   dba_hist_sqltext
 where  sql_id = '&sqlid'
+and    instance_number = sys_context('USERENV','INSTANCE')
 and    rownum < 2;
 
 prompt
@@ -23,7 +24,8 @@ prompt
 select max(snap_id) snap_id
 from   dba_hist_sqlstat
 where  sql_id = '&sqlid'
-and    plan_hash_value = &phv;
+and    plan_hash_value = &phv
+and    instance_number = sys_context('USERENV','INSTANCE');
 
 prompt 
 prompt Loading the plan from AWR
@@ -38,12 +40,14 @@ begin
    into   esnap
    from   dba_hist_sqlstat
    where  sql_id = '&sqlid'
-   and    plan_hash_value = &phv;
+   and    plan_hash_value = &phv
+   and    instance_number = sys_context('USERENV','INSTANCE');
 
    select max(snap_id)
    into   bsnap
    from   dba_hist_snapshot
-   where  snap_id < esnap;
+   where  snap_id < esnap
+   and    instance_number = sys_context('USERENV','INSTANCE');
 
    if esnap is null
    then
@@ -75,7 +79,4 @@ prompt
 select signature, sql_handle, plan_name, accepted, fixed, substr(sql_text,1,55) || '...' sql_text
 from  dba_sql_plan_baselines
 where signature = :sig;
-
-   
-
 
